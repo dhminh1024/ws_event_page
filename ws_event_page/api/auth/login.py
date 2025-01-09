907b45e812af485fa75cf910c34fde943555e83b
Minh Do <dhminh1024@gmail.com>
2025-01-09 10:09:17 +0700

    fix: auth with WS code for staff return full_name instead of fullName

--- ws_event_page/api/auth/login.py
+++ ws_event_page/api/auth/login.py
@@ -37,8 +37,8 @@ def login_with_wellspring_code(wellspring_code):
         staff_person = frappe.get_doc("SIS Person", staff.person).as_dict()
 
         return {
-            "user_type": "Staff",
-            "user_data": {
+            "userType": "Staff",
+            "userData": {
                 "fullName": staff_person.full_name,
                 "email": staff_person.email,
                 "gender": staff_person.gender,
