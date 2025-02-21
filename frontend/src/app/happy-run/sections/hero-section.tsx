6e3bcbdeeea116c460f1e89b3dd9e3b8b0111e95
KhaNguyen24 <kha.nguyen@wellspring.edu.vn>
2025-02-21 16:43:09 +0700

    Update feedback

--- frontend/src/app/happy-run/sections/hero-section.tsx
+++ frontend/src/app/happy-run/sections/hero-section.tsx
@@ -1,17 +1,45 @@
 import { useEventPageContext } from "@/lib/event-page/use-event-page";
-import { HTMLAttributes, type FC } from "react";
+import { HTMLAttributes, useEffect, useRef, type FC } from "react";
 
 export type HeroSectionProps = HTMLAttributes<HTMLDivElement> & {
   className?: string;
 };
 
-export const HeroSection: FC<HeroSectionProps> = ({ className,...props }) => {
+export const HeroSection: FC<HeroSectionProps> = ({ className, ...props }) => {
   const event = useEventPageContext();
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = true;
+      videoRef.current.play();
+      videoRef.current.addEventListener("loadedmetadata", () => {
+        if (videoRef.current) {
+          console.log("Loaded");
+          videoRef.current.muted = true;
+          videoRef.current.play();
+        }
+      });     
+    }
+  }, []);
 
   return (
     <div className="relative w-full bg-hr-blue overflow-hidden" {...props}>
-      <video className="relative z-20 w-full" src={event.variables.hero_kv?.value} autoPlay muted loop/>
-      <img className="w-full absolute top-0 left-0 z-5" src={event.variables.hero_image?.value} alt="hero" />
+      <video
+        ref={videoRef}
+        className="relative z-20 w-full"
+        poster={event.variables.hero_image?.value}
+        src={event.variables.hero_kv?.value}
+        autoPlay
+        muted
+        loop
+        playsInline
+      />
+      <img
+        className="w-full absolute top-0 left-0 z-5"
+        src={event.variables.hero_image?.value}
+        alt="hero"
+      />
     </div>
   );
 };
