c9a25b92ecfcab64556e61d74ce97fcd21b3f07c
KhaNguyen24 <kha.nguyen@wellspring.edu.vn>
2025-02-24 11:25:43 +0700

    Update placement-test

--- frontend/src/app/placement-test/global.ts
+++ frontend/src/app/placement-test/global.ts
@@ -3,18 +3,18 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   :root {
     --sidebar-width: 16rem;
-    font-size: calc(1px + (100vw - 375px) / 375 * 1);
+    /* font-size: calc(1px + (100vw - 375px) / 375 * 1); */
   }
 
   body{
     font-family: "Mulish", sans-serif;
   }
 
-  @media (min-width: 768px) {
+  /* @media (min-width: 768px) {
     :root {
       font-size: calc(1px + (100vw - 1440px) / 1440);
     }
-  }
+  } */
 
 `;
 
--- frontend/src/app/placement-test/page.tsx
+++ frontend/src/app/placement-test/page.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEventPageContext } from "@/lib/event-page/use-event-page";
 import { useEffect } from "react";
 import Typography from "./components/typography";
-import { Calendar } from "./components/calendar";
+import { Calendar } from "@atoms/calendar";
 
 export const Component = () => {
   const navigate = useNavigate();
@@ -19,7 +19,7 @@ export const Component = () => {
   return (
     <div className="bg-pt-background overflow-hidden h-[100vh]">
       <div className="flex justify-center items-center h-full">
-        <div className="w-[90%] md:w-[70%] h-[90vh] m-auto bg-white rounded-[10rem] overflow-hidden">
+        <div className="w-[90%] md:w-[70%] h-[90vh] m-auto bg-white rounded-5 overflow-hidden">
           <div className="flex h-full w-full">
             <div className="basis-[25%] bg-slate-400"></div>
             <div className="basis-[50%] bg-slate-200">
@@ -27,7 +27,7 @@ export const Component = () => {
                 mode="single"
                 // selected={date}
                 // onSelect={setDate}
-                className="rounded-md border text-hr-primary"
+                className="rounded-md w-full border text-hr-primary"
               />
             </div>
             <div className="basis-[25%] bg-slate-300"></div>
