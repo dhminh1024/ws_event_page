ecc418f9705eeaa19ae012358b5307ceaa33b025
Minh Do <dhminh1024@gmail.com>
2025-02-27 16:06:09 +0700

    happy-run: add api for autocomplete school class and department

--- frontend/src/app/happy-run/api/use-check-user-by-code.ts
+++ frontend/src/app/happy-run/api/use-check-user-by-code.ts
@@ -1,11 +1,13 @@
-import { useFrappeGetCall, useFrappePostCall } from 'frappe-react-sdk';
-import { useState, useEffect } from 'react';
-import { FRAPPE_APIS } from './api.config';
-import { HRUserInfo } from '../types/variables';
+import { useFrappeGetCall, useFrappePostCall } from "frappe-react-sdk";
+import { useState, useEffect } from "react";
+import { FRAPPE_APIS } from "./api.config";
+import { HRUserInfo } from "../types/variables";
 
 const useCheckUserByCode = () => {
-const { call } = useFrappePostCall<{message: HRUserInfo }>(FRAPPE_APIS.GET_USER_BY_CODE.METHOD_STRING);
-  
+  const { call } = useFrappePostCall<{ message: HRUserInfo }>(
+    FRAPPE_APIS.GET_USER_BY_CODE.METHOD_STRING
+  );
+
   return { getInfo: call };
 };
 
