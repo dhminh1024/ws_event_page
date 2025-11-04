5585dbf0f07444b20968134f0eb2e326d5091d61
KhaNguyen24 <kha.nguyen@wellspringsaigon.edu.vn>
2025-11-04 09:10:34 +0700

    Upgrade GA

--- frontend/src/app/greatest-show-25/root.tsx
+++ frontend/src/app/greatest-show-25/root.tsx
@@ -7,9 +7,14 @@ import ReactGA from "react-ga4";
 import { Toaster, toast } from "sonner";
 
 const GAID = "G-ERLLHRP52G"; // Replace with your Google Analytics ID
-ReactGA.initialize(GAID);
-ReactGA.send({ hitType: "pageview" });
-
+if (GAID) {
+  ReactGA.initialize(GAID)
+  ReactGA.send({
+    hitType: 'pageview',
+    page: window.location.pathname + window.location.search,
+    title: 'Greatest Show',
+  })
+}
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <EventPageProvider eventUrl={EVENT_PAGES.GREATEST_SHOW_25.SITE_URL}>
